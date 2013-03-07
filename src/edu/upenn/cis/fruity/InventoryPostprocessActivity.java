cb8ea64284751370752db9d272dd6fea10a13030
Elizabeth <soelizab@sas.upenn.edu>
2013-03-07 17:52:39 -0500

    Add methods to parse inputs and save data to database.

--- src/edu/upenn/cis/fruity/InventoryPostprocessActivity.java
+++ src/edu/upenn/cis/fruity/InventoryPostprocessActivity.java
@@ -1,10 +1,13 @@
 package edu.upenn.cis.fruity;
 
+import edu.upenn.cis.fruity.database.DatabaseHandler;
+import edu.upenn.cis.fruity.database.FruitStand;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * User can input quantities for whole fruit, mixed bags, and other food inventory
@@ -12,7 +15,13 @@ import android.view.View;
  */
 public class InventoryPostprocessActivity extends Activity {
 	
+	private int numApples, numPears, numOranges, numBananas, numGrapes, numKiwis;
+	private int numMixedBags, numFrozenFruitBags, numGranola;
+	private double applePrice, pearPrice, orangePrice, bananaPrice, grapesPrice, kiwiPrice;
+	private double mixedBagPrice, frozenFruitBagPrice, granolaPrice;	
 	public static final int InventoryPostprocessActivity_ID = 10;
+	
+	private ParseInputData parser = new ParseInputData();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +36,64 @@ public class InventoryPostprocessActivity extends Activity {
 		return true;
 	} 
 	
+	/**
+	 * When click on button, parse input and save to database
+	 * @param view
+	 */
 	public void onInventorySalesDemographicsButtonClick(View view){
+		parseNumberProcessedInventoryInput();
+		parsePricePerProcessedInventoryItemInput();
+		saveProcessedInventoryData();
+		
 		Intent i = new Intent(this,SalesDemographicActivity.class);
 		startActivityForResult(i, InventoryPostprocessActivity_ID);
+	}
+	
+	/**
+	 * Parse the number of processed inventory items from input
+	 */
+	public void parseNumberProcessedInventoryInput(){
+		numApples = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_apple));
+		numPears = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_pear));
+		numOranges = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_orange));
+		numBananas = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_banana));
+		numGrapes = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_grapes));
+		numKiwis = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_kiwi));
+		numMixedBags = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_mixedBag));
+		numFrozenFruitBags = parser.parseItemNum((EditText)findViewById(R.id.num_inv_postprocess_frozenFruitBag));
+		numGranola = parser.parseItemNum((EditText) findViewById(R.id.num_inv_postprocess_granola));
+	}
+	
+	/**
+	 * Parse the price per inventory item from input
+	 */
+	public void parsePricePerProcessedInventoryItemInput(){
+		applePrice = parser.parseItemPrice((EditText)findViewById(R.id.price_apple));
+		pearPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_pear));
+		orangePrice = parser.parseItemPrice((EditText)findViewById(R.id.price_orange));
+		bananaPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_banana));
+		grapesPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_grapes));
+		kiwiPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_kiwi));
+		mixedBagPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_mixedBag));
+		frozenFruitBagPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_frozenFruitBag));
+		granolaPrice = parser.parseItemPrice((EditText)findViewById(R.id.price_granola));
+	}
+	
+	/**
+	 * Save the number and price per processed inventory item to the database
+	 */
+	public void saveProcessedInventoryData(){
+		DatabaseHandler dh = DatabaseHandler.getInstance(this);
+		FruitStand currentStand = dh.getCurrentFruitStand();
+		
+		currentStand.addProcessedInventoryItem(this, "apple", numApples, applePrice);
+		currentStand.addProcessedInventoryItem(this, "pear", numPears, pearPrice);
+		currentStand.addProcessedInventoryItem(this, "orange", numOranges, orangePrice);
+		currentStand.addProcessedInventoryItem(this, "banana", numBananas, bananaPrice);
+		currentStand.addProcessedInventoryItem(this, "grapes", numGrapes, grapesPrice);
+		currentStand.addProcessedInventoryItem(this, "kiwi", numKiwis, kiwiPrice);
+		currentStand.addProcessedInventoryItem(this, "mixedBag", numMixedBags, mixedBagPrice);
+		currentStand.addProcessedInventoryItem(this, "frozenFruitBag", numFrozenFruitBags, frozenFruitBagPrice);
+		currentStand.addProcessedInventoryItem(this, "granola", numGranola, granolaPrice);
 	}
 }
--- src/edu/upenn/cis/fruity/InventoryPreprocessActivity.java
+++ src/edu/upenn/cis/fruity/InventoryPreprocessActivity.java
@@ -1,18 +1,23 @@
 package edu.upenn.cis.fruity;
 
+import edu.upenn.cis.fruity.database.DatabaseHandler;
+import edu.upenn.cis.fruity.database.FruitStand;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * User can input quantities for whole fruit inventory before fruit is processed and sold
  */
 public class InventoryPreprocessActivity extends Activity {
-	
+	private int numApples, numPears, numOranges, numBananas, numGrapes, numKiwis;
 	public static final int InventoryPreprocessActivity_ID = 9;
 
+	private ParseInputData parser = new ParseInputData();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,7 +32,36 @@ public class InventoryPreprocessActivity extends Activity {
 	} 
 	
 	public void onInventoryPostprocessButtonClick(View view){
+	
+		
 		Intent i = new Intent(this,InventoryPostprocessActivity.class);
 		startActivityForResult(i, InventoryPreprocessActivity_ID);
+	}
+	
+	/**
+	 * Parse the number of start inventory items from input, before processing
+	 */
+	public void parseNumberStartInventoryInput(){
+		numApples = parser.parseItemNum((EditText)findViewById(R.id.num_inv_preprocess_apple));
+		numPears = parser.parseItemNum((EditText)findViewById(R.id.num_inv_preprocess_pear));
+		numOranges = parser.parseItemNum((EditText)findViewById(R.id.num_inv_preprocess_orange));
+		numBananas = parser.parseItemNum((EditText)findViewById(R.id.num_inv_preprocess_banana));
+		numGrapes = parser.parseItemNum((EditText)findViewById(R.id.num_inv_preprocess_grapes));
+		numKiwis = parser.parseItemNum((EditText)findViewById(R.id.num_inv_preprocess_kiwi));
+	}
+	
+	/**
+	 * Save the number of each start inventory item to the database
+	 */
+	public void saveStartInventoryData(){
+		DatabaseHandler dh = DatabaseHandler.getInstance(this);
+		FruitStand currentStand = dh.getCurrentFruitStand();
+		
+		currentStand.addStartInventoryItem(this, "apple", numApples);
+		currentStand.addStartInventoryItem(this, "pear", numPears);
+		currentStand.addStartInventoryItem(this, "orange", numOranges);
+		currentStand.addStartInventoryItem(this, "banana", numBananas);
+		currentStand.addStartInventoryItem(this, "grapes", numGrapes);
+		currentStand.addStartInventoryItem(this, "kiwi", numKiwis);		
 	}
 }
